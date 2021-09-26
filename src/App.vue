6e99b99ef0ee6e51d1ba694042b0d463d2a7930e
LoelDev <84813731+LoelDev@users.noreply.github.com>
2021-09-26 12:06:46 -0500

    Seleccionar si eres stremaer o gamer, falta el resto

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app color="indigo darken-1 " dark >
+      <v-app-bar-title class="white--text " >User Type</v-app-bar-title>
+    </v-app-bar>
+    <v-main class="indigo lighten-2">
+     <sg-selection/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import SgSelection from "@/components/sgSelection";
+
+export default {
+  name: 'App',
+
+  components: {
+    SgSelection
+  },
+
+  data: () => ({
+
+  }),
+};
+</script>
---
+++ src/views/gamerProfileStreamer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar app color="indigo darken-1 " dark >
+      <v-app-bar-title class="white--text " >User Type</v-app-bar-title>
+    </v-app-bar>
+    <v-main class="indigo lighten-2">
+      <sg-selection/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "gamerProfileStreamer"
+}
+</script>
+
+<style scoped>
+
+</style>
